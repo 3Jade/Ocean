56ffd3e772a9866fe863ac08f72be0353192cd30
ShadauxCat <shadowcat.github@phasethrough.com>
2015-05-26 23:57:04 -0500

    Added re2 directory that had been missed before and is required for building, removed extraneous backup directory.

--- third-party/sprawl_bak/include/sprawl/common/specialized.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace sprawl
-{
-	template<size_t i>
-	struct Specialized {};
-}
--- third-party/sprawl_bak/include/sprawl/string/StringCommon.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../common/compat.hpp"
-#include <cstring>
-
-#ifndef SPRAWL_STATIC_STRING_SIZE
-#	define SPRAWL_STATIC_STRING_SIZE 128
-#endif
